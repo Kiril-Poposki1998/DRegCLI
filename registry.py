068910f933143dd8bb9ddf00abb69bc0d3067441
KIRIL <kirilpoposki@hotmail.com>
2023-04-15 11:39:31 +0200

    improve get images and add listing tags

--- registry.py
+++ registry.py
@@ -1,5 +1,6 @@
 from requests import get, exceptions
 from os import system, name
+import json
 
 class Registry:
     def __init__(self, url):
@@ -8,21 +9,42 @@ class Registry:
 
     def connect(self):
         try:
-            data = get(self.url + "/v2/_catalog")
-            print(data.text)
+            data = get(self.url + "/v2/")
+            if data.text is not None:
+                print("Connected to",self.url)
         except (exceptions.HTTPError, exceptions.ConnectionError) as error:
             print(error.args[0])
 
     def get_repo_images(self):
         try:
             data = get(self.url + "/v2/_catalog")
-            print(data.text)
+            json_object = json.loads(data.text)
+            images = json_object["repositories"]
+            if len(images) == 0:
+                print("No images are in the registry")
+            else:
+                for i in range(len(images)):
+                    print("\t"+str(i+1)+".",images[i])
+        except (exceptions.HTTPError, exceptions.ConnectionError) as error:
+            print(error.args[0])
+
+    def list_tags(self):
+        try:
+            if self.image_name is not None:
+                data = get(self.url + "/v2/"+self.image_name+"/tags/list")
+                json_object = json.loads(data.text)
+                tags = json_object["tags"]
+                for i in range(len(tags)):
+                    print("\t"+str(i+1)+".",tags[i])
+            else:
+                print("No image name set")
         except (exceptions.HTTPError, exceptions.ConnectionError) as error:
             print(error.args[0])
 
     def get_help(self):
         print("""
         get_images - get all images stored in registry\n
+        list_tags - list tags associated with the name of the image\n
         set *image name* - set the image to inspect\n
         exit - Either CTRL+C or type this to exit\n
         clear - Clears the screen\n
@@ -53,6 +75,9 @@ class Registry:
 
     def command_handler(self, cmd):
         match cmd:
+            case "list_tags":
+                self.list_tags()
+                return
             case "get_images":
                 self.get_repo_images()
                 return
